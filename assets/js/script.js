e022a989073983259746731b7cf8c04611488c45
sammaxfleet <>
2023-07-17 15:35:56 +0000

    user choice logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,21 @@
-// Wait for the DOM to finish loading before running the game
-// Get the button elements and add event listeners to them
-
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
-
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.dataset.selection === "submit") {
-                alert("You clicked!");
-            } else {
-                let gameType = this.dataset.selection;
-                alert(`You clicked ${gameType}`);
-            }
-        });
-    }
-
-});
-
-function runGame() {
-  const selectionButtons = document.querySelectorAll('[button-selection]')
-const SELECTIONS = [
-
-  {
-    name: 'rock',
-    emoji: '🤛',
-    beats: [
-        'scissors', 'lizard'
-  ]
-},
-{
-    name: 'paper',
-    emoji: ' ✋',
-    beats: [
-        'rock', 'spock'
-    ]
-},
-
-  {
-    name: 'scissors',
-    Symbol: '✂️',
-    beats: [
-        'paper', 'lizard'
-    ]
-},
-{
-  name: 'lizard' ,
-  Symbol: '🦎',
-  beats: [
-    'paper', 'spock'
-]
-},
-{
-  name: 'spock' ,
-  Symbol: '🖖',
-  beats: [
-    'scissors ' , 'rock'
-  ]
+// function to reset the game and display the winner
+function resetGame(winner) {
+    document.body.innerHTML = `the ${winner} has won.`;
 }
-]}
-
-function makeSelection(selection) {
-  // Creates 1 random numbers between 1 and 5
-  let num1 = Math.floor(Math.random() * 5) + 1;
-
-  const computerSelection = randomSelection()
-  const computerWinner = isWinner(computerSelection, selection)
-  const yourWinner = isWinner(selection, computerSelection)
-  
-  addSelectionResult(computerSelection, computerWinner)
-  addSelectionResult(selection, yourWinner)
-
-  if (yourWinner) incrementScore(yourScoreSpan)
-  if (computerWinner) incrementScore(computerScoreSpan)
+// Function to play the game
+function playGame(userChoice)
+// choices available
+let choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+// randomly chosing for AI
+let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+// variables to display results
+let userScore = '';
+let AIScore = '';
+let resultText = '';
+// for tie
+if (userChoice === computerChoice) {
+    resultText = "It's a tie";
 }
-
-  function incrementScore(scoreSpan)
-{
-  scoreSpan.Result (score-area)
-
-}
-
 
 
 
