1650e0f94a2cbdc8bbf16710e59bdfce3de5c2d0
sammaxfleet <>
2023-07-19 12:36:41 +0000

    added media queries for width of game_Area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,57 @@
-
 // function to remove body elements and display the winner
 function resetGame(winner) {
-    document.body.innerHTML = `the ${winner} has won.<br><button class="return_btn"><a href="/">Back to Game</a></button>`;
-
+  document.body.innerHTML = `the ${winner} has won.<br><button class="return_btn"><a href="/">Back to Game</a></button>`;
 }
 // Function to play the game
 function playGame(userChoice) {
-// choices available
-let choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
-// randomly chosing for AI using random() and floor() to round off the number to nearest integer
-let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  // choices available
+  let choices = ["rock", "paper", "scissors", "lizard", "spock"];
+  // randomly chosing for AI using random() and floor() to round off the number to nearest integer
+  let computerChoice = choices[Math.floor(Math.random() * choices.length)];
 
-// Variables to display results
-let userScore = '';
-let AIScore = '';
-let resultText = '';
+  // Variables to display results
+  let resultText = "";
 
-// For a tie
-if (userChoice === computerChoice) {
+  // For a tie
+  if (userChoice === computerChoice) {
     resultText = "It's a tie";
-}
-// User Wins conditions
-else if (
-(userChoice === 'rock' && (computerChoice === 'scissors' || computerChoice === 'lizard')) ||
-(userChoice === 'paper' && (computerChoice === 'rock' || computerChoice === 'spock')) ||
-(userChoice === 'scissors' && (computerChoice === 'paper' || computerChoice === 'lizard')) ||
-(userChoice === 'lizard' && (computerChoice === 'paper' || computerChoice === 'spock')) ||
-(userChoice === 'spock' && (computerChoice === 'rock' || computerChoice === 'scissors'))
-) 
+  }
+  // User Wins conditions
+  else if (
+    (userChoice === "rock" &&
+      (computerChoice === "scissors" || computerChoice === "lizard")) ||
+    (userChoice === "paper" &&
+      (computerChoice === "rock" || computerChoice === "spock")) ||
+    (userChoice === "scissors" &&
+      (computerChoice === "paper" || computerChoice === "lizard")) ||
+    (userChoice === "lizard" &&
+      (computerChoice === "paper" || computerChoice === "spock")) ||
+    (userChoice === "spock" &&
+      (computerChoice === "rock" || computerChoice === "scissors"))
+  ) {
+    // update results accordingly for user winner conditions
 
-// update results accordingly for user winner conditions
-
-{
     resultText = "You Win!";
-    let currentResult = document.getElementById('userScore').value += '+ 1';
+    let currentResult = (document.getElementById("userScore").value += "+ 1");
     let userScored = eval(currentResult);
     if (userScored > 9) {
-        resetGame('User');
+      resetGame("User");
     } else {
-        document.getElementById('userScore').value = userScored;
+      document.getElementById("userScore").value = userScored;
     }
-}
-// update results according to AI winner conditions
-else {
+  }
+  // update results according to AI winner conditions
+  else {
     resultText = "You Lose!";
-    let currentResultAI = document.getElementById('AIScore').value += '+ 1';
+    let currentResultAI = (document.getElementById("AIScore").value += "+ 1");
     let AIScored = eval(currentResultAI);
     if (AIScored > 9) {
-        resetGame('AI');
+      resetGame("AI");
     } else {
-        document.getElementById('AIScore').value = AIScored;
+      document.getElementById("AIScore").value = AIScored;
     }
+  }
+  document.getElementById(
+    "result"
+  ).textContent = `you chose ${userChoice}. Computer Chose ${computerChoice}. ${resultText}`;
 }
-document.getElementById('result').textContent = `you chose ${userChoice}. Computer Chose ${computerChoice}. ${resultText}`;
-} 
